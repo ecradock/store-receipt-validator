ad49150e4249578825880fa7b21fb83d55fbf711
Ed Cradock <ed@privax.com>
2016-10-06 17:57:45 +0100

    Should be addScope - not setScope

--- src/ReceiptValidator/GooglePlay/Client/CertificateClient.php
+++ src/ReceiptValidator/GooglePlay/Client/CertificateClient.php
@@ -31,7 +31,7 @@ class CertificateClient extends Client
         $client->setClientId($config->getClientId());
         $client->setAuthConfig($config->getJsonPath());
         $client->setAccessType('offline');
-        $client->setScope(self::ANDROID_PUBLISHER_URI);
+        $client->addScope(self::ANDROID_PUBLISHER_URI);
 
         $this->client = $client;
         $this->config = $config;
